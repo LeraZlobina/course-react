def1a434873ed6a283ffefba4703f0624889bb5b
Lera Zlobina <leras__zlobina@mail.ru>
2021-05-18 01:46:26 +0500

    Удалены лишние комментарии

--- loft-taxi/src/store.js
+++ loft-taxi/src/store.js
@@ -1,17 +1,14 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./modules";
-//import { saveState, loadState } from "./localStorage";
 import createSagaMiddleware from "redux-saga";
 import {sagas} from "./sagas"
 
 const sagaMiddleware = createSagaMiddleware();
 
-//const persistedState = loadState();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
     rootReducer,
-    //persistedState,
     composeEnhancers (
         applyMiddleware(sagaMiddleware),
     )
@@ -19,13 +16,5 @@ export const store = createStore(
 
 sagaMiddleware.run(sagas);
 
-/*store.subscribe(
-    () => {
-        saveState({
-            isLoggedIn: store.getState().isLoggedIn,
-            isRegisterIn: store.getState().isRegisterIn,
-            isSubmitted: store.getState().isSubmitted
-        })
-    }
-);*/
+
 
